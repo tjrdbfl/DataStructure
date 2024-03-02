4338c4e7664561cbaa6601c4919c6dc5a9fede78
tjrdbfl <tjrdbfl123@gmail.com>
2024-03-02 15:50:36 +0900

    initial commit

---
+++ src/BaekjoonStack/Baekjoon9012.java
@@ -0,0 +1,33 @@
+package BaekjoonStack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Baekjoon9012 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String str=br.readLine();
+            Stack<Character> stack=new Stack<>();
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '(') stack.push(str.charAt(j));
+                else{
+                    if(stack.empty()){
+                        stack.push(str.charAt(j));
+                        break;
+                    }else{
+                        stack.pop();
+                    }
+                }
+            }
+            if (stack.empty()) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
